9c935be600c6ed95345de632fdabed028e8978ad
Leticia Lumi Nagao <leticia-nagao@hotmail.com>
2022-05-22 18:46:13 -0300

    finishing touches

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -8,7 +8,7 @@ const Auth = () => {
       <div className="a-left">
         <img src={Logo} alt="" />
         <div className="Webname">
-          <h1>ZKC Media</h1>
+          <h1>Milu Media</h1>
           <h6>Explore the ideas throughout the world</h6>
         </div>
       </div>
